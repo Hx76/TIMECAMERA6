83ec3bc2b37d606648154fe741b0af3491eba5ce
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-06-26 20:59:28 +0800

    地图

--- app/src/main/java/com/example/timecamera/FootActivity.java
+++ app/src/main/java/com/example/timecamera/FootActivity.java
@@ -1,13 +1,30 @@
 package com.example.timecamera;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class FootActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext());
-        setContentView(R.layout.activity_foot);
+import android.app.Activity;
+import android.os.Bundle;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class FootActivity extends Activity {
+    MapView mMapView = null;
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            SDKInitializer.initialize(getApplicationContext());
+            setContentView(R.layout.activity_foot);
+            mMapView =  findViewById(R.id.bmapView);
+        }
+        protected void onDestroy() {
+            super.onDestroy();
+            mMapView.onDestroy();
+        }
+        protected void onResume() {
+            super.onResume();
+            mMapView.onResume();
+        }
+        protected void onPause() {
+            super.onPause();
+            mMapView.onPause();
+        }
     }
 
-}
