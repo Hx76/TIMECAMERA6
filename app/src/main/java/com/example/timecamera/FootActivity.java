74804da2dfc722f9b9da577caa5e6613d0c042e5
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-06-29 19:20:31 +0800

    实现了拍照显示缩略图

--- app/src/main/java/com/example/timecamera/FootActivity.java
+++ app/src/main/java/com/example/timecamera/FootActivity.java
@@ -1,30 +1,103 @@
 package com.example.timecamera;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.MapView;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 
 public class FootActivity extends Activity {
-    MapView mMapView = null;
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            SDKInitializer.initialize(getApplicationContext());
-            setContentView(R.layout.activity_foot);
-            mMapView =  findViewById(R.id.bmapView);
+    private ImageView mimageview;
+    private static int REQ = 1;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foot);
+        mimageview = findViewById(R.id.imageView);
+        Button takephoto = (Button) findViewById(R.id.capture);
+        takephoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File outputImage = new File(getExternalCacheDir(),"outputImage.jpg");
+                try {
+                if (outputImage.exists()) {
+                    outputImage.delete();
+                }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUri = FileProvider.getUriForFile(FootActivity.this,"com.example.timecamera.fileprovider",outputImage);
+                }else {
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+
+        public void lookinfo(View view) {
+            Intent intent = new Intent();
+            intent.setClass(FootActivity.this,InfoActivity.class);
+            startActivity(intent);
         }
-        protected void onDestroy() {
-            super.onDestroy();
-            mMapView.onDestroy();
-        }
-        protected void onResume() {
-            super.onResume();
-            mMapView.onResume();
-        }
-        protected void onPause() {
-            super.onPause();
-            mMapView.onPause();
+
+    public void takephoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQ);
+    }
+    public void select(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//调用android的图库
+        startActivityForResult(intent,REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK ) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        mimageview.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
         }
     }
+}
 
