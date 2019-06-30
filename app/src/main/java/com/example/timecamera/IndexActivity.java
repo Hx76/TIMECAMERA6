192fcce1e60ebda3664320fd33fbbfac94da5b0e
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-06-30 20:37:26 +0800

    实现了调用相机拍照保存到手机

--- app/src/main/java/com/example/timecamera/IndexActivity.java
+++ app/src/main/java/com/example/timecamera/IndexActivity.java
@@ -1,21 +1,85 @@
 package com.example.timecamera;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.view.View;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class IndexActivity extends AppCompatActivity {
+    private static int REQ = 1;
+    private String mFilePaths;
+    @RequiresApi(api = Build.VERSION_CODES.M-1)
+    void Request() {             //获取相机拍摄读写权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//版本判断
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA}, 1);
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        mFilePaths = Environment.getExternalStorageDirectory().getPath();
+        mFilePaths = mFilePaths + "/" + "temp.png";
+        Request();
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
     }
     public void StartFoot(View view) {
         Intent intent = new Intent();
         intent.setClass(IndexActivity.this,FootActivity.class);
         startActivity(intent);
     }
+    public void StartCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoUri = Uri.fromFile(new File(mFilePaths));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+        startActivityForResult(intent,REQ);
+    }
+    public void StartPhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//调用android的图库
+        startActivity(intent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            FileInputStream fis = null;
+            if (requestCode == REQ) {
+                try {
+                    fis = new FileInputStream(mFilePaths);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 
 }
